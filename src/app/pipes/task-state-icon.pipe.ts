4af6f30aa4af9a09442dd91c9abc33c40598fd27
Klaster_1 <Klaster1@gmail.com>
2019-09-20 19:54:58 +0300

    wip

--- src/app/pipes/task-state-icon.pipe.ts
+++ src/app/pipes/task-state-icon.pipe.ts
@@ -7,14 +7,12 @@ import {Task} from '@app/types'
 export class TaskStateIconPipe {
     transform(t?: Task | string) {
         if (!t) return 'timer-logo'
+        if (typeof t !== 'string' && !!t.sessions && !!t.sessions.length && !t.sessions[t.sessions.length-1].end) {
+            return 'pause_circle_filled'
+        }
         const v = typeof t === 'string' ? t : t.state
         switch (v) {
-            case 'active':
-                return typeof t === 'string'
-                    ? 'play_circle_filled'
-                    : !!t.sessions && !!t.sessions.length && !t.sessions[t.sessions.length-1].end
-                        ? 'pause_circle_filled'
-                        : 'play_circle_filled'
+            case 'active': return 'play_circle_filled'
             case 'done': return 'check_circled'
             case 'dropped': return 'delete'
             case 'on-hold': return 'low_priority'
