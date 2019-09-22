d39e92b8d63b9f90eff05fcf4cf94c11880c132a
Klaster_1 <Klaster1@gmail.com>
2019-09-22 11:16:08 +0300

    wip

--- src/app/pipes/format-duration.pipe.ts
+++ src/app/pipes/format-duration.pipe.ts
@@ -1,11 +1,20 @@
 import { Pipe, PipeTransform, NgModule } from '@angular/core';
+import {Observable} from 'rxjs';
+import {distinctUntilChanged, map} from 'rxjs/operators';
 const pad = (value: number) => (value || 0).toString().length === 1 ? '0' + value : value
 
 @Pipe({
   name: 'formatDuration'
 })
 export class FormatDurationPipe implements PipeTransform {
-    transform(value: number) {
+    transform(value: number): string
+    transform(value: Observable<number>): Observable<string>
+    transform(value: number | Observable<number>) {
+        return typeof value === 'number'
+            ? this.format(value)
+            : value.pipe(map(v=>this.format(v)), distinctUntilChanged())
+    }
+    format(value: number) {
         if (value <= 0) return ''
 
         return ['h','m'].map((part) => {
@@ -16,7 +25,7 @@ export class FormatDurationPipe implements PipeTransform {
                     return pad(~~((value % 3600000) / 60000))
             }
         }).join(':')
-    }
+}
 }
 
 @NgModule({declarations: [FormatDurationPipe], exports: [FormatDurationPipe]})
