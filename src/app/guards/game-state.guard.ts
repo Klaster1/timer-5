0b55525eef768455c5dd45fc408e2e353b7b9d8b
Klaster_1 <Klaster1@gmail.com>
2019-10-24 22:44:55 +0300

    wip

---
+++ src/app/guards/game-state.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { isValidTaskState } from '@app/domain';
+
+@Injectable({providedIn: 'root'})
+export class GameStateGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const gamesState = next.params.state
+    return isValidTaskState(gamesState) || gamesState === 'all'
+      ? true
+      : this.router.navigate(['/tasks/active'])
+  }
+}
