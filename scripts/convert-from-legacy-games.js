d60e77e7310b60d3b36a6b3029ab3056f93c0fcc
Klaster_1 <Klaster1@gmail.com>
2019-10-30 00:35:23 +0300

    wip

---
+++ scripts/convert-from-legacy-games.js
@@ -0,0 +1,64 @@
+// @ts-check
+
+const fs = require('fs')
+const path = require('path')
+const minimist = require('minimist')
+const shortid = require('shortid')
+// import {StoreState, Task, TaskState} from '../src/app/types'
+
+/**
+ * @typedef {import('../src/app/types').StoreState} StoreState
+ */
+/**
+ * @typedef {import('../src/app/types').TaskState} TaskState
+ */
+
+/**
+ * @typedef {{
+  state: 'active'|'finished'|'dropped'|'hold'|'wish',
+  title: string,
+  id: string,
+  sessions: {start: number, stop: number}[]
+}} Game
+ */
+
+/**
+ * @param {Game[]} games
+ * @returns {StoreState['tasks']}
+ */
+const gamesToTasks = (games) => {
+  const tasks = Object.fromEntries(games.map(game => {
+    const id = shortid()
+    return [
+      id,
+      {
+        id,
+        name: game.title,
+         /** @type {any} */
+        state: ({
+          active: 'active',
+          finished: 'done',
+          dropped: 'dropped',
+          hold: 'on-hold',
+          wish: 'to-do'
+        })[game.state],
+        sessions: game.sessions.map(s => ({
+          id: shortid(),
+          start: s.start,
+          stop: s.stop
+        }))
+      }
+    ]
+  }))
+  return {
+    ids: Object.keys(tasks),
+    values: tasks
+  }
+}
+
+const { games, tasks } = minimist(process.argv.slice(2))
+const tasksObject = gamesToTasks(JSON.parse(fs.readFileSync(path.resolve(games), 'utf8')))
+
+fs.writeFileSync(path.resolve(tasks), JSON.stringify(tasksObject, null, '  '))
+
+console.log(`Converted ${tasksObject.ids.length} games to tasks.`)
