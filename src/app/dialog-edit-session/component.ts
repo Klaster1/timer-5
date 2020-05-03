a0e3628d2e27408b1cdba7cab9700366e61c609e
Klaster_1 <Klaster1@gmail.com>
2020-05-03 12:22:04 +0300

    improve session edit dialog

--- src/app/dialog-edit-session/component.ts
+++ src/app/dialog-edit-session/component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, Control } from '@ng-stack/forms';
 
 export interface DialogEditSessionData {
   start: number;
@@ -8,14 +9,23 @@ export interface DialogEditSessionData {
 
 @Component({
   selector: 'dialog-edit-session',
-  templateUrl: './template.html',
+  templateUrl: './component.html',
+  styleUrls: ['./component.scss'],
 })
 export class DialogEditSessionComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogEditSessionData,
+    private fb: FormBuilder,
     private dialog: MatDialogRef<DialogEditSessionComponent, DialogEditSessionData>
   ) {}
-  onSubmit(start: number, end: number) {
-    this.dialog.close({ start, end: Number.isNaN(end) ? undefined : end });
+  form = this.fb.group<{ start: Control<Date>; end: Control<Date> }>({
+    start: this.fb.control(new Date(this.data.start)),
+    end: this.fb.control(this.data.end ? new Date(this.data.end) : undefined),
+  });
+  onSubmit() {
+    this.dialog.close({
+      start: this.form.value.start.valueOf(),
+      end: Number.isNaN(this.form.value.end?.valueOf()) ? undefined : this.form.value.end.valueOf(),
+    });
   }
 }
--- src/app/dialog-edit-session/index.ts
+++ src/app/dialog-edit-session/index.ts
@@ -1,13 +1,30 @@
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+  NgxMatTimepickerModule,
+} from '@angular-material-components/datetime-picker';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { NgStackFormsModule } from '@ng-stack/forms';
 import { DialogEditSessionComponent, DialogEditSessionData } from './component';
 export { DialogEditSessionComponent, DialogEditSessionData };
 
 @NgModule({
-  imports: [MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgStackFormsModule,
+    MatDatepickerModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+    NgxMatTimepickerModule,
+  ],
   declarations: [DialogEditSessionComponent],
   exports: [DialogEditSessionComponent],
   entryComponents: [DialogEditSessionComponent],
