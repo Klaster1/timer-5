209fae2e001d706741caa1ba40a541935e87bbea
Klaster_1 <Klaster1@gmail.com>
2020-12-20 16:52:11 +0300

    compact form conversion

---
+++ scripts/convert-to-compact-form.ts
@@ -0,0 +1,74 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+import yargs from 'yargs';
+import { StoreState, TaskState } from '../src/app/types';
+import sizeof from './sizeof';
+
+type Branded<T, U> = T & { brand: U };
+type TaskId = Branded<string, 'TaskId'>;
+enum CompactTaskState {
+  active,
+  done,
+  dropped,
+}
+type SessionStartTimestampInSeconds = Branded<number, 'SessionStartTimestamp'>;
+type SessionDurationInSeconds = Branded<number, 'SessionDuration'>;
+type TaskSession = [SessionStartTimestampInSeconds] | [SessionStartTimestampInSeconds, SessionDurationInSeconds];
+type TaskName = Branded<string, 'TaskName'>;
+enum TaskKey {
+  name,
+  state,
+  sessions,
+}
+type TaskCompact = {
+  [TaskKey.name]: TaskName;
+  [TaskKey.state]: CompactTaskState;
+  [TaskKey.sessions]: TaskSession[];
+};
+type TasksCompactDocument = Record<TaskId, TaskCompact>;
+const toSeconds = (ms: number): number => Math.round(ms / 1000);
+
+const nextTaskId = (tasks: TasksCompactDocument): TaskId => (Object.keys(tasks).unshift() + 1).toString(10) as TaskId;
+
+const tasksToCompactTasks = (tasks: StoreState['tasks']): TasksCompactDocument =>
+  Object.values(tasks.values).reduce(
+    (acc, task): TasksCompactDocument => ({
+      ...acc,
+      [nextTaskId(acc)]: {
+        [TaskKey.name]: task.name as TaskName,
+        [TaskKey.state]:
+          task.state === TaskState.active
+            ? CompactTaskState.active
+            : task.state === TaskState.done
+            ? CompactTaskState.done
+            : CompactTaskState.dropped,
+        [TaskKey.sessions]: task.sessions.map((s) =>
+          s.end
+            ? [
+                toSeconds(s.start) as SessionStartTimestampInSeconds,
+                toSeconds(s.end - s.start) as SessionDurationInSeconds,
+              ]
+            : [toSeconds(s.start) as SessionStartTimestampInSeconds]
+        ),
+      } as TaskCompact,
+    }),
+    {}
+  );
+
+const { input, output } = yargs(process.argv).options({
+  input: { type: 'string', required: true },
+  output: { type: 'string', required: true },
+}).argv;
+
+console.log('Input', input);
+console.log('Output', output);
+
+const data = JSON.parse(readFileSync(resolve(input), 'utf8'));
+
+const result = tasksToCompactTasks(data);
+
+writeFileSync(resolve(output), JSON.stringify(result, null, '  '));
+
+console.log(`Converted ${Object.keys(result).length} tasks to compact tasks.`);
+console.log(`Firestore input size: ${sizeof(data)}`);
+console.log(`Firestore output size: ${sizeof(result)}`);
---
+++ scripts/sizeof.ts
@@ -0,0 +1,82 @@
+'use strict';
+
+// get bytes in string
+// https://stackoverflow.com/a/23329386/591487
+function byteLength(str: string) {
+  // returns the byte length of an utf8 string
+  var s = str.length;
+  for (var i = str.length - 1; i >= 0; i--) {
+    var code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s += 2;
+  }
+  return s;
+}
+
+function sizeOfObject(object: any) {
+  if (object === null) {
+    return 1;
+  }
+  //GeoPoint
+  if (object.hasOwnProperty('_lat') && typeof object.isEqual === 'function') return 16;
+  //Date
+  if (typeof object.toDate === 'function' || typeof object.getDate === 'function') return 8;
+  //Blob
+  if (typeof object.toUint8Array === 'function') {
+    var blob = object.toUint8Array();
+    return blob.byteLength;
+  }
+  //Reference
+  if (typeof object.onSnapshot === 'function') {
+    return object['_key']['path']['segments']
+      .slice(object['_key']['path']['offset'])
+      .reduce(function (acc: number, curr: string) {
+        return acc + curr.length + 1;
+      }, 16);
+  }
+  var bytes = 0;
+  for (var key in object) {
+    if (!Object.hasOwnProperty.call(object, key)) {
+      continue;
+    }
+
+    bytes += sizeof(key);
+    try {
+      bytes += sizeof(object[key]);
+    } catch (ex) {
+      if (ex instanceof RangeError) {
+        // circular reference detected, final result might be incorrect
+        // let's be nice and not throw an exception
+        bytes = 0;
+      }
+    }
+  }
+
+  return bytes;
+}
+
+function sizeof(object: unknown): number {
+  switch (typeof object) {
+    case 'string':
+      return byteLength(object) + 1;
+    case 'boolean':
+      return 1;
+    case 'number':
+      return 8;
+    case 'object':
+      if (Array.isArray(object)) {
+        return object.map(sizeof).reduce(function (acc, curr) {
+          return acc + curr;
+        }, 0);
+      } else {
+        return sizeOfObject(object);
+      }
+    default:
+      return 0;
+  }
+}
+
+function sizeOfDoc(object: unknown) {
+  return 32 + sizeof(object);
+}
+export default sizeOfDoc;
