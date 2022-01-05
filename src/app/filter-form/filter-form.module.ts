98685eca587bddd47bc9acc3a7fa5f66dc828505
Klaster_1 <Klaster1@gmail.com>
2022-01-05 15:52:16 +0300

    Configure filter in module.

--- src/app/filter-form/filter-form.module.ts
+++ src/app/filter-form/filter-form.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FilterFormDirective } from './filter-form.directive';
 import { FilterFormService } from './filter-form.service';
+import { FilterFormsSettings, FILTER_FORM_SETTINGS } from './types';
 
 @NgModule({
   declarations: [FilterFormDirective],
   exports: [FilterFormDirective],
   providers: [FilterFormService],
 })
-export class FilterFormModule {}
+export class FilterFormModule {
+  static forChild<T extends object>(settings: FilterFormsSettings<T>): ModuleWithProviders<FilterFormModule> {
+    return {
+      ngModule: FilterFormModule,
+      providers: [
+        {
+          provide: FILTER_FORM_SETTINGS,
+          useValue: settings,
+        },
+      ],
+    };
+  }
+}
