8eb43a4ed7489c00a96abfdf8a96eb619afad551
Klaster_1 <Klaster1@gmail.com>
2022-01-05 21:40:55 +0300

    Improve task filtering.

--- src/app/domain/filter.ts
+++ src/app/domain/filter.ts
@@ -1,29 +1,45 @@
 import { Task, TasksFilterParams } from '../types/domain';
 
-const filterByName = (filter: TasksFilterParams, t: Task): boolean => {
+type Nullable<T> = T | null;
+
+const filterByName = (filter: TasksFilterParams, t: Nullable<Task>): Nullable<Task> => {
+  if (!t) return t;
   const { search } = filter;
   if (typeof search === 'string' && search.length) {
-    return t.name.toLowerCase().includes(search.toLowerCase());
+    return t.name.toLowerCase().includes(search.toLowerCase()) ? t : null;
   } else {
-    return true;
+    return t;
   }
 };
-const filterByFrom = (filter: TasksFilterParams, t: Task): boolean => {
+const filterByFrom = (filter: TasksFilterParams, t: Nullable<Task>): Nullable<Task> => {
+  if (!t) return t;
   const { from } = filter;
   if (from instanceof Date && !Number.isNaN(from.valueOf())) {
-    return t.sessions.some((s) => s.start >= from.valueOf());
+    const sessions = t.sessions.filter((s) => s.start >= from.valueOf());
+    return sessions.length ? { ...t, sessions } : null;
   } else {
-    return true;
+    return t;
   }
 };
-const filterByTo = (filter: TasksFilterParams, t: Task): boolean => {
+const filterByTo = (filter: TasksFilterParams, t: Nullable<Task>): Nullable<Task> => {
+  if (!t) return t;
   const { to } = filter;
   if (to instanceof Date && !Number.isNaN(to.valueOf())) {
-    return t.sessions.some((s) => (typeof s.end === 'number' ? s.end <= to.valueOf() : true));
+    const sessions = t.sessions.filter((s) => (typeof s.end === 'number' ? s.end <= to.valueOf() : true));
+    return sessions.length ? { ...t, sessions } : null;
   } else {
-    return true;
+    return t;
   }
 };
 
+const composedPredicate = (filter: TasksFilterParams, t: Nullable<Task>): Nullable<Task> =>
+  filterByTo(filter, filterByFrom(filter, filterByName(filter, t)));
+
 export const filter = (filter: TasksFilterParams, values: Task[]): Task[] =>
-  values.filter((t) => [filterByName, filterByTo, filterByFrom].every((predicate) => predicate(filter, t)));
+  values.reduce((acc: Task[], task) => {
+    const result = composedPredicate(filter, task);
+    if (result) {
+      acc.push(result);
+    }
+    return acc;
+  }, []);
