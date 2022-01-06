74b5da921e5bd0d1d18d77a4a933a6cdb5adf625
Klaster_1 <Klaster1@gmail.com>
2022-01-06 12:25:53 +0300

    Stricter date parsing.

--- src/app/screen-tasks/index.ts
+++ src/app/screen-tasks/index.ts
@@ -94,7 +94,8 @@ export type TasksFilterRouteParams = {
             return [key, rawValue];
           case 'from':
           case 'to':
-            return [key, new Date(rawValue)];
+            const dateRe = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
+            return dateRe.test(rawValue) ? [key, new Date(rawValue)] : null;
           case 'durationSort': {
             switch (rawValue) {
               case 'longestFirst':
