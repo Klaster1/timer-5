432e35184351a268bf2c78cb6d61b30f9aa6c6be
Klaster_1 <Klaster1@gmail.com>
2022-01-11 18:15:28 +0300

    Allow to submit a session edit without the "End".
    Fixes #7.

--- src/app/dialog-edit-session/dialog-edit-session.component.ts
+++ src/app/dialog-edit-session/dialog-edit-session.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Control, FormControl, FormGroup } from '@ng-stack/forms';
+import { Control, FormControl, FormGroup, Validators } from '@ng-stack/forms';
 
 export interface DialogEditSessionData {
   start: number;
@@ -19,13 +19,14 @@ export class DialogEditSessionComponent {
     private dialog: MatDialogRef<DialogEditSessionComponent, DialogEditSessionData>
   ) {}
   form = new FormGroup<{ start: Control<Date>; end: Control<Date> }>({
-    start: new FormControl(new Date(this.data.start)),
+    start: new FormControl(new Date(this.data.start), [Validators.required]),
     end: new FormControl(this.data.end ? new Date(this.data.end) : undefined),
   });
   onSubmit() {
-    this.dialog.close({
-      start: this.form.value.start.valueOf(),
-      end: Number.isNaN(this.form.value.end?.valueOf()) ? undefined : this.form.value.end.valueOf(),
-    });
+    if (this.form.valid)
+      this.dialog.close({
+        start: this.form.value.start.valueOf(),
+        end: this.form.value.end?.valueOf(),
+      });
   }
 }
