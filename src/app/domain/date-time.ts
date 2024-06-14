cf5b2d40a638bbacfe7eb5078bf8c5575394bf8a
Klaster_1 <Klaster1@gmail.com>
2024-06-14 19:26:40 +0300

    Drop eslint.

--- src/app/domain/date-time.ts
+++ src/app/domain/date-time.ts
@@ -12,10 +12,8 @@ export const formatHours = (value: Duration): string => {
     .map((part) => {
       switch (part) {
         case 'h':
-          // eslint-disable-next-line no-bitwise
           return pad2(~~(value / 3600000));
         case 'm':
-          // eslint-disable-next-line no-bitwise
           return pad2(~~((value % 3600000) / 60000));
         default:
           return assertNever(part);
