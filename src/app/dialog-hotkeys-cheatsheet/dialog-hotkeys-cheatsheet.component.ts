2d8ed00849ba6aa8b1ed5f93a03d5c9191cb75d6
Klaster_1 <Klaster1@gmail.com>
2024-06-15 09:25:40 +0300

    Modernize dialog-hotkeys-cheatsheet.

--- src/app/dialog-hotkeys-cheatsheet/dialog-hotkeys-cheatsheet.component.ts
+++ src/app/dialog-hotkeys-cheatsheet/dialog-hotkeys-cheatsheet.component.ts
@@ -1,7 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MapPipe } from '@app/pipes/map.pipe';
 import { hasCyrillics } from '@app/utils/string';
 import { HotkeysService } from 'angular2-hotkeys';
@@ -12,11 +11,11 @@ import { HotkeysService } from 'angular2-hotkeys';
   styleUrls: ['./dialog-hotkeys-cheatsheet.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, CommonModule, MapPipe],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButton, MapPipe],
 })
 export class DialogHotkeysCheatsheetComponent {
   static ID = 'DIALOG_HOTKEYS_CHEATSHEET';
-  constructor(private hotkeysService: HotkeysService) {}
+  private hotkeysService = inject(HotkeysService);
   keys = this.hotkeysService.hotkeys.filter((key) => key.description);
   withoutCyrillics = (values: string[]): string[] => [...values].filter((value) => !hasCyrillics(value));
 }
