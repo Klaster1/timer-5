784425f91409238ecd2afa1e87c46a0fce9d92d9
Klaster_1 <Klaster1@gmail.com>
2024-06-15 09:22:07 +0300

    Modernize dialog-edit-session.

--- src/app/dialog-edit-session/dialog-edit-session.component.ts
+++ src/app/dialog-edit-session/dialog-edit-session.component.ts
@@ -1,10 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 import { DatetimeLocalDirective } from '@app/directives/datetime-local.directive';
 
 export interface DialogEditSessionData {
@@ -19,20 +25,23 @@ export interface DialogEditSessionData {
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButton,
+    MatFormField,
+    MatError,
+    MatLabel,
+    MatInput,
     ReactiveFormsModule,
-    CommonModule,
     DatetimeLocalDirective,
   ],
 })
 export class DialogEditSessionComponent {
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: DialogEditSessionData,
-    private dialog: MatDialogRef<DialogEditSessionComponent, DialogEditSessionData>
-  ) {}
+  public data = inject<DialogEditSessionData>(MAT_DIALOG_DATA);
+  private dialog = inject<MatDialogRef<DialogEditSessionComponent, DialogEditSessionData>>(MatDialogRef);
+
   form = new FormGroup<{ start: FormControl<Date>; end: FormControl<Date | null> }>({
     start: new FormControl(new Date(this.data.start), { validators: [Validators.required], nonNullable: true }),
     end: new FormControl(this.data.end ? new Date(this.data.end) : null),
