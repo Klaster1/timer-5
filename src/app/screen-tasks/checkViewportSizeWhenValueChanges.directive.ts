ce14f25b6861d6e668d49e81a9300998432f1429
Klaster_1 <Klaster1@gmail.com>
2024-06-15 10:11:39 +0300

    Modernize CheckViewportSizeWhenValueChangesDirective.

--- src/app/screen-tasks/checkViewportSizeWhenValueChanges.directive.ts
+++ src/app/screen-tasks/checkViewportSizeWhenValueChanges.directive.ts
@@ -1,14 +1,17 @@
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
-import { Directive, Input } from '@angular/core';
+import { Directive, effect, inject, input } from '@angular/core';
 
 @Directive({
   selector: '[checkViewportSizeWhenValueChanges]',
   standalone: true,
 })
 export class CheckViewportSizeWhenValueChangesDirective {
-  constructor(private viewport: CdkVirtualScrollViewport) {}
-  @Input()
-  set checkViewportSizeWhenValueChanges(val: any) {
-    setTimeout(() => this.viewport.checkViewportSize());
+  private viewport = inject(CdkVirtualScrollViewport);
+  public checkViewportSizeWhenValueChanges = input<any>();
+  constructor() {
+    effect(() => {
+      this.checkViewportSizeWhenValueChanges();
+      this.viewport.checkViewportSize();
+    });
   }
 }
