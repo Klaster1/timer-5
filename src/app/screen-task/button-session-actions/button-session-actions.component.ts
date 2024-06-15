9b737e25e6c4af1f0e84ea3e5cd85b6e1660ccab
Klaster_1 <Klaster1@gmail.com>
2024-06-15 09:40:18 +0300

    Modernize button-session-actions.

--- src/app/screen-task/button-session-actions/button-session-actions.component.ts
+++ src/app/screen-task/button-session-actions/button-session-actions.component.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSelectModule } from '@angular/material/select';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuContent, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 import { encodeFilterParams } from '@app/domain/router';
 import { StoreState } from '@app/domain/storage';
-import { getSessionId, Session, Task } from '@app/domain/task';
+import { Session, Task, getSessionId } from '@app/domain/task';
 import * as actions from '@app/ngrx/actions';
 import { Store } from '@ngrx/store';
 
@@ -16,29 +15,34 @@ import { Store } from '@ngrx/store';
   selector: 'button-session-actions',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule, MatSelectModule, MatIconModule, RouterModule],
+  imports: [MatMenu, MatMenuContent, MatMenuItem, MatMenuTrigger, MatIconButton, MatIcon, RouterLink],
 })
 export class ButtonSessionActionsComponent {
-  constructor(private store: Store<StoreState>) {}
-  @Input() task?: Task;
-  @Input() session?: Session;
+  private store = inject<Store<StoreState>>(Store);
+  public task = input<Task>();
+  public session = input<Session>();
   edit() {
-    if (this.task && this.session)
+    const task = this.task();
+    const session = this.session();
+    if (task && session)
       this.store.dispatch(
         actions.updateSessionIntent({
-          taskId: this.task.id,
-          sessionId: getSessionId(this.session),
-        })
+          taskId: task.id,
+          sessionId: getSessionId(session),
+        }),
       );
   }
   remove() {
-    if (this.task && this.session)
-      this.store.dispatch(actions.deleteSession({ taskId: this.task.id, sessionId: getSessionId(this.session) }));
+    const task = this.task();
+    const session = this.session();
+    if (task && session)
+      this.store.dispatch(actions.deleteSession({ taskId: task.id, sessionId: getSessionId(session) }));
   }
   get skipBeforeParams() {
-    return this.session ? encodeFilterParams({ from: new Date(this.session.start) }) : {};
+    const session = this.session();
+    return session ? encodeFilterParams({ from: new Date(session.start) }) : {};
   }
   get skipAfterParams() {
-    return encodeFilterParams({ to: new Date(this.session?.end ?? new Date()) });
+    return encodeFilterParams({ to: new Date(this.session()?.end ?? new Date()) });
   }
 }
