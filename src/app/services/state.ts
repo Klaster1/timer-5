a7d731412820fa46f2fb50bc9ea9b371909501ab
Klaster_1 <Klaster1@gmail.com>
2024-06-16 21:02:41 +0300

    Add tasks feature.

--- src/app/services/state.ts
+++ src/app/services/state.ts
@@ -1,8 +1,11 @@
 import { DestroyRef, computed, effect, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
-import { Theme } from '@app/domain/storage';
+import { NormalizedTasks, Theme } from '@app/domain/storage';
+import { Session, SessionId, TaskState, isSessionWithId, isTaskRunning } from '@app/domain/task';
+import { deepEquals } from '@app/utils/assert';
 import {
+  StateSignal,
   patchState,
   signalStore,
   signalStoreFeature,
@@ -11,7 +14,11 @@ import {
   withMethods,
   withState,
 } from '@ngrx/signals';
-import { produce } from 'immer';
+import { Draft, produce } from 'immer';
+
+const updateState = <State extends object>(store: StateSignal<State>, recipe: (draft: Draft<State>) => any) => {
+  patchState(store, (state) => produce(state, recipe));
+};
 
 const withRouter = () => {
   type RouterState = {
@@ -91,15 +98,13 @@ const withTheme = () => {
       const router = inject(Router);
       return {
         toggleTheme(theme?: Theme) {
-          patchState(store, (state) =>
-            produce(state, (draft) => {
-              if (theme) {
-                draft.theme = theme;
-              } else {
-                draft.theme = draft.theme === 'dark' ? 'light' : 'dark';
-              }
-            }),
-          );
+          updateState(store, (draft) => {
+            if (theme) {
+              draft.theme = theme;
+            } else {
+              draft.theme = draft.theme === 'dark' ? 'light' : 'dark';
+            }
+          });
         },
       };
     }),
@@ -114,4 +119,89 @@ const withTheme = () => {
   );
 };
 
-export const AppStore = signalStore({ providedIn: 'root' }, withTheme(), withRouter());
+const withTasks = () => {
+  type TasksState = {
+    tasks: NormalizedTasks;
+  };
+  const initialState: TasksState = {
+    tasks: {},
+  };
+  return signalStoreFeature(
+    withState<TasksState>(initialState),
+    withMethods((store) => ({
+      stopTask(taskId: string, timestamp: number) {
+        updateState(store, (draft) => {
+          store.tasks()[taskId]?.sessions.forEach((session) => {
+            if (typeof session.end !== 'number') session.end = timestamp;
+          });
+        });
+      },
+      updateSession(taskId: string, sessionId: SessionId, start: number, end: number) {
+        updateState(store, (draft) => {
+          const session = store.tasks()[taskId]?.sessions.find(isSessionWithId(sessionId));
+          if (session) {
+            session.start = start;
+            session.end = end;
+          }
+        });
+      },
+      deleteSession(taskId: string, sessionId: SessionId) {
+        updateState(store, (draft) => {
+          const task = store.tasks()[taskId];
+          if (!task) return;
+          const index = task?.sessions.findIndex(isSessionWithId(sessionId));
+          if (index !== -1) task.sessions.splice(index, 1);
+        });
+      },
+      loadTasks(tasks: NormalizedTasks) {
+        updateState(store, (draft) => {
+          draft.tasks = tasks;
+        });
+      },
+      createTask(taskId: string, name: string) {
+        updateState(store, (draft) => {
+          draft.tasks[taskId] = { id: taskId, name, sessions: [], state: TaskState.active };
+        });
+      },
+      renameTask(taskId: string, name: string) {
+        updateState(store, (draft) => {
+          const task = draft.tasks[taskId];
+          if (task) task.name = name;
+        });
+      },
+      updateTaskState(taskId: string, state: TaskState) {
+        updateState(store, (draft) => {
+          const task = draft.tasks[taskId];
+          if (task) task.state = state;
+        });
+      },
+      deleteTask(taskId: string) {
+        updateState(store, (draft) => {
+          delete draft.tasks[taskId];
+        });
+      },
+      startTask(taskId: string, timestamp: number) {
+        updateState(store, (draft) => {
+          const task = draft.tasks[taskId];
+          if (task) {
+            task.state = TaskState.active;
+            task.sessions.push({ start: timestamp });
+          }
+        });
+      },
+      moveSessionToTask(fromTaskId: string, toTaskId: string, session: Session) {
+        updateState(store, (draft) => {
+          const fromTask = draft.tasks[fromTaskId];
+          const toTask = draft.tasks[toTaskId];
+          if (fromTask && toTask) {
+            fromTask.sessions = fromTask.sessions.filter((session) => !deepEquals(session, session));
+            toTask.sessions.push(session);
+            if (isTaskRunning(toTask)) toTask.state = TaskState.active;
+          }
+        });
+      },
+    })),
+  );
+};
+
+export const AppStore = signalStore({ providedIn: 'root' }, withTheme(), withRouter(), withTasks());
