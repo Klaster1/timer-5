cbcd13dbc926dba10fc8074880a1f647819c1519
Klaster_1 <Klaster1@gmail.com>
2024-06-18 14:22:04 +0100

    Auto-generated commit

---
+++ chunk-U4TP6YLV.js
@@ -0,0 +1,2 @@
+import{t as e}from"./chunk-VGRJF5ZH.js";var t=r=>(r||0).toString().length===1?"0"+r:r;var i=r=>r<=0?"00:00":["h","m"].map(o=>{switch(o){case"h":return t(~~(r/36e5));case"m":return t(~~(r%36e5/6e4));default:return e(o)}}).join(":");export{i as a};
+//# sourceMappingURL=chunk-U4TP6YLV.js.map
