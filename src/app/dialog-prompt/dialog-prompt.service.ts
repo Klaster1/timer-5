7af1fe37fef87c8432942ec7562361e2fe7b6bc1
Klaster_1 <Klaster1@gmail.com>
2024-06-18 16:09:25 +0300

    Don't lazy load the prompt dialog component.

--- src/app/dialog-prompt/dialog-prompt.service.ts
+++ src/app/dialog-prompt/dialog-prompt.service.ts
@@ -1,16 +1,15 @@
 import { Injectable, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { lastValueFrom } from 'rxjs';
-import type { DialogPromptData } from './dialog-prompt.component';
+import DialogPromptComponent, { DialogPromptData } from './dialog-prompt.component';
 
 @Injectable({ providedIn: 'root' })
 export class Prompt {
   private dialog = inject(MatDialog);
   async prompt(title: string, value = '', placeholder = ''): Promise<string | undefined> {
-    const component = await import('./dialog-prompt.component').then((m) => m.default);
     return lastValueFrom(
       this.dialog
-        .open(component, {
+        .open(DialogPromptComponent, {
           data: { title, value, placeholder } as DialogPromptData,
         })
         .afterClosed(),
