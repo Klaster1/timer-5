a29139f78342917ab261faa37f3217f4850f2eba
Klaster_1 <Klaster1@gmail.com>
2024-06-22 11:40:37 +0300

    Make deepEquals generic.

--- src/app/utils/assert.ts
+++ src/app/utils/assert.ts
@@ -4,6 +4,6 @@ export const assertNever = (value: never): never => {
   throw new Error(`This should never happen: ${value}`);
 };
 
-export const deepEquals = (a: any, b: any): boolean => JSON.stringify(a) === JSON.stringify(b);
+export const deepEquals = <T>(a: T, b: T): boolean => JSON.stringify(a) === JSON.stringify(b);
 
 export const isNumber = (value: any): value is number => typeof value === 'number';
