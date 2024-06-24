754f80e8fa939d42c61c17d3e27ef6d39f5ea463
Klaster_1 <Klaster1@gmail.com>
2024-06-24 15:10:56 +0300

    Use local-web-server to serve instead of express.

--- scripts/gh-pages-before-add.js
+++ scripts/gh-pages-before-add.js
@@ -1,12 +1,12 @@
 import { copyFile, writeFile } from 'node:fs/promises';
 
-await copyFile('dist/index.html', 'dist/404.html');
+await copyFile('dist/timer-5/index.html', 'dist/timer-5/404.html');
 console.log('404 created');
 
-const ngswConfig = (await import('../dist/ngsw.json', { assert: { type: 'json' } })).default;
+const ngswConfig = (await import('../dist/timer-5/ngsw.json', { assert: { type: 'json' } })).default;
 const indexHtmlAt = ngswConfig.assetGroups.at(0).urls.indexOf(`/timer-5/index.html`);
 ngswConfig.assetGroups.at(0).urls.splice(indexHtmlAt, 0, '/timer-5/404.html');
 const indexHtmlHash = ngswConfig.hashTable['/timer-5/index.html'];
 ngswConfig.hashTable['/timer-5/404.html'] = indexHtmlHash;
-await writeFile('dist/ngsw.json', JSON.stringify(ngswConfig, null, '  '));
+await writeFile('dist/timer-5/ngsw.json', JSON.stringify(ngswConfig, null, '  '));
 console.log('ngsw.json updated');
