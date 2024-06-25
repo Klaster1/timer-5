490ccb00a970322c1aee89bd034bf602fc62c5fb
Klaster_1 <Klaster1@gmail.com>
2024-06-25 07:10:07 +0300

    Draft VR tools.

---
+++ e2e/visual-regression.ts
@@ -0,0 +1,64 @@
+import { existsSync, rmSync } from 'node:fs';
+import path from 'node:path';
+import { ODiffOptions, compare as odiff } from 'odiff-bin';
+import slugify from 'slugify';
+import { t } from 'testcafe';
+
+type VisualRegressionMode = 'create' | 'compare';
+
+export const VISUAL_REGRESSION_OK = { match: true } as const;
+export const VISUAL_REGRESSION_META = { 'visual-regression': 'true' };
+
+type ScreenshotPathName = 'reference' | 'current' | 'diff';
+type ScreenshotPaths = Record<ScreenshotPathName, string>;
+
+export const isVisualRegressionEnabled = () =>
+  (t.test.meta as Partial<typeof VISUAL_REGRESSION_META>)['visual-regression'] === 'true';
+
+const s = (value: string) => slugify(value).replace(/["':`]/g, '');
+
+const getPaths = (name: string): { relative: ScreenshotPaths; absolute: ScreenshotPaths } => {
+  const BASE_PATH = ['screenshots', 'visual-regression'];
+
+  const commonFileName = `${s(t.fixture.name)}__${s(t.test.name)}__${s(name)}`;
+
+  const reference = path.join(...BASE_PATH, `${commonFileName}.reference.png`);
+  const current = path.join(...BASE_PATH, `${commonFileName}.current.png`);
+  const diff = path.join(...BASE_PATH, `${commonFileName}.diff.png`);
+
+  return {
+    relative: { reference, current, diff },
+    absolute: {
+      reference: path.resolve(__dirname, reference),
+      current: path.resolve(__dirname, current),
+      diff: path.resolve(__dirname, diff),
+    },
+  };
+};
+
+export async function comparePageScreenshot(
+  name: string,
+  options?: {
+    odiff?: ODiffOptions;
+  },
+): ReturnType<typeof odiff> {
+  if (!process.argv.some((arg) => arg === 'visual-regression=true')) return await VISUAL_REGRESSION_OK;
+  if (!isVisualRegressionEnabled()) return await VISUAL_REGRESSION_OK;
+
+  const paths = getPaths(name);
+  const mode: VisualRegressionMode = existsSync(paths.absolute.reference) ? 'compare' : 'create';
+
+  if (mode === 'create') {
+    await t.takeScreenshot({ path: path.relative('screenshots', paths.relative.reference), fullPage: true });
+  } else if (mode === 'compare') {
+    rmSync(paths.absolute.diff, { force: true });
+    await t.takeScreenshot({ path: path.relative('screenshots', paths.relative.current), fullPage: true });
+  }
+  if (mode === 'create') {
+    return VISUAL_REGRESSION_OK;
+  } else if (mode === 'compare') {
+    return await odiff(paths.absolute.reference, paths.absolute.current, paths.absolute.diff);
+  } else {
+    return VISUAL_REGRESSION_OK;
+  }
+}
