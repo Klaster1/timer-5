af1e5202893bd791a04409fb1a781a0895baf4e6
Klaster_1 <Klaster1@gmail.com>
2024-06-26 15:13:51 +0300

    Adjust duration dimming.

--- src/app/directives/duration.component.ts
+++ src/app/directives/duration.component.ts
@@ -5,10 +5,16 @@ import { DurationFn, Milliseconds } from '@app/domain/date-time';
 import { pad2 } from '@app/utils/number';
 import { Observable, combineLatest, interval, map, shareReplay, startWith, switchMap } from 'rxjs';
 
+enum DimMode {
+  All,
+  First,
+  None,
+}
+
 type Fragment = {
   value: string;
   unit: Unit;
-  dimmed: boolean;
+  dimmed: DimMode;
 };
 
 enum Unit {
@@ -21,8 +27,14 @@ enum Unit {
   selector: 'duration',
   template: `
     @for (fragment of durationFragments | async; track fragment.unit) {
-      <span class="fragment" [ngClass]="{ dimmed: fragment.dimmed }"
-        ><span class="value">{{ fragment.value }}</span
+      <span class="fragment" [ngClass]="{ dimmed: fragment.dimmed === DimMode.All }"
+        ><span class="value">
+          @if (fragment.dimmed === DimMode.First) {
+            <span class="dimmed">{{ fragment.value.at(0) }}</span
+            ><span>{{ fragment.value.slice(1) }}</span>
+          } @else {
+            {{ fragment.value }}
+          }</span
         ><span class="unit">{{ fragment.unit }}</span></span
       >
     }
@@ -37,6 +49,10 @@ enum Unit {
       .unit {
         font-size: var(--unit-font-size);
       }
+      .value {
+        display: inline-flex;
+        align-items: baseline;
+      }
       .dimmed {
         opacity: 0.35;
       }
@@ -49,17 +65,23 @@ enum Unit {
 export class DurationComponent {
   private interval = inject(DurationIntervalDirective).interval;
   public readonly value = input.required<DurationFn>();
+  public readonly DimMode = DimMode;
   public readonly durationFragments = combineLatest([toObservable(this.value), this.interval]).pipe(
     map(([value, now]): Fragment[] => {
       const hours = ~~(value(now) / 3600000);
       const minutes = ~~((value(now) % 3600000) / 60000);
       const seconds = ~~((value(now) % 60000) / 1000);
       if (hours === 0 && minutes === 0) {
-        return [{ value: seconds.toString(), unit: Unit.Seconds, dimmed: false }];
+        return [{ value: seconds.toString(), unit: Unit.Seconds, dimmed: DimMode.None }];
       }
       return [
-        { value: hours.toString(), unit: Unit.Hours, dimmed: hours === 0 },
-        { value: pad2(minutes), unit: Unit.Minutes, dimmed: hours === 0 && minutes === 0 },
+        { value: hours.toString(), unit: Unit.Hours, dimmed: hours === 0 ? DimMode.All : DimMode.None },
+        {
+          value: pad2(minutes),
+          unit: Unit.Minutes,
+          dimmed:
+            hours === 0 && minutes === 0 ? DimMode.All : minutes < 10 && hours === 0 ? DimMode.First : DimMode.None,
+        },
       ];
     }),
   );
