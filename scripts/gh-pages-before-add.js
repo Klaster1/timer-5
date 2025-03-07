603a3f8f7ac01fbf282698b456bbca0ce8649edc
Klaster_1 <Klaster1@gmail.com>
2025-03-07 12:06:31 +0200

    Fix the build.

--- scripts/gh-pages-before-add.js
+++ scripts/gh-pages-before-add.js
@@ -3,7 +3,7 @@ import { copyFile, writeFile } from 'node:fs/promises';
 await copyFile('dist/timer-5/index.html', 'dist/timer-5/404.html');
 console.log('404 created');
 
-const ngswConfig = (await import('../dist/timer-5/ngsw.json', { assert: { type: 'json' } })).default;
+const ngswConfig = (await import('../dist/timer-5/ngsw.json', { with: { type: 'json' } })).default;
 const indexHtmlAt = ngswConfig.assetGroups.at(0).urls.indexOf(`/timer-5/index.html`);
 ngswConfig.assetGroups.at(0).urls.splice(indexHtmlAt, 0, '/timer-5/404.html');
 const indexHtmlHash = ngswConfig.hashTable['/timer-5/index.html'];
